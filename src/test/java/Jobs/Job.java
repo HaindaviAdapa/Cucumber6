1316c2e071f2197075527a15c122fd96e41ac138
HaindaviAdapa <haindavi1234@gmail.com>
2023-04-19 19:45:44 +0530

    my fourteenth commit

---
+++ src/test/java/Jobs/Job.java
@@ -0,0 +1,25 @@
+package Jobs;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Job
+{
+	
+	@Then("User opens application URL{string}")
+	public void user_opens_application_url_https_opensource_demo_orangehrmlive_com_web_index_php_auth_login(String TestURL) {
+
+}
+
+}
